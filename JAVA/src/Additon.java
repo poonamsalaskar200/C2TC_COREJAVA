4da3091924ecaec9aed6304e4dc6b75a9d28323e
HP <HP@DESKTOP-GB5R2CB>
2022-03-08 19:31:23 +0530

    first commit

---
+++ JAVA/src/Additon.java
@@ -0,0 +1,12 @@
+
+public class Additon {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+        int a= 20;
+        int b= 10;
+        int c= a+b;
+        System.out.println("The Addition of a & b is" +c);
+	}
+
+}
